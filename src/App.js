246e8d08fe3dcb77d8de8e1cf7a447269a07fe18
Julia Cano <juliacanoflor@gmail.com>
2024-12-02 18:48:20 +0100

    pagina inicial

--- src/App.js
+++ src/App.js
@@ -4,37 +4,17 @@ import Carrusel from './Carrusel';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <nav className="navbar">
-          <ul className="nav-links">
-            <li><a href="#home">Home</a></li>
-            <li><a href="#about">About</a></li>
-            <li><a href="#services">Services</a></li>
-            <li><a href="#contact">Contact</a></li>
-          </ul>
-        </nav>
-      </header>
-      <main>
-      <div className="carrusel">
-            <Carrusel/>
-        </div>
-        <section id="home">
-          <h1>Welcome to My Website</h1>
-          <p>This is the Home section.</p>
-        </section>
-        <section id="about">
-          <h1>About Us</h1>
-          <p>This is the About section.</p>
-        </section>
-        <section id="services">
-          <h1>Our Services</h1>
-          <p>This is the Services section.</p>
-        </section>
-        <section id="contact">
-          <h1>Contact Us</h1>
-          <p>This is the Contact section.</p>
-        </section>
-      </main>
+      <div className="App-header">
+          <div classname="shop-name">
+            <p>Ropa y complementos Lorenzo</p>
+          </div>        
+      </div>
+      <div className="main">
+          <Carrusel/>
+      </div>
+      <div className="footer"> 
+        <p>Dirección: Calle Mayor 8, Almendricos, Murcia | Teléfono: +34 617 588 867</p>
+      </div>    
     </div>
   );
 }
--- src/Carrusel.js
+++ src/Carrusel.js
@@ -9,11 +9,9 @@ import { Autoplay } from 'swiper/modules'; // Modulo para autoplay
 
 const Carrusel = () => {
     const imagenes = [
-      'https://cdn.pixabay.com/photo/2023/05/15/14/35/cat-7995231_640.jpg?text=Imagen+1',
-      'https://cdn.pixabay.com/photo/2013/04/01/03/45/cat-98359_960_720.jpg?text=Imagen+2',
-      'https://c.pxhere.com/photos/2e/93/cat_pet_cat_face_black_and_white_eyes_animal_feline_black_and_white_cat-910723.jpg!d?text=Imagen+3',
-      'https://images.unsplash.com/photo-1708033956915-219592082ad8?fm=jpg&q=60&w=3000&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8Z2F0byUyMGRlJTIwaW50ZXJpb3J8ZW58MHx8MHx8fDA%3D?text=Imagen+4',
-      'https://images.pexels.com/photos/2527491/pexels-photo-2527491.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1?text=Imagen+5',
+      'https://cdn.palbincdn.com/users/8901/images/IMG_2086-1719899874.jpeg?text=Imagen+1',
+      'https://i0.wp.com/www.lacasitademartina.com/wp-content/uploads/2015/02/Zippy-moda-infantil-Ropa-Ni%C3%B1os-Blog-Moda-Infantil-La-casita-de-Martina-Personal-Shopper-1.jpg?text=Imagen+2',
+      'https://m.media-amazon.com/images/S/aplus-media/sota/154ea1b4-5d12-44ea-b8cd-b73570debda0.__CR0,0,970,600_PT0_SX970_V1___.jpg?text=Imagen+3',
     ];
   
     return (
@@ -38,57 +36,3 @@ const Carrusel = () => {
   };
   
   export default Carrusel;
-
-
-/*import React, { useState, useEffect } from "react";
-
-const Carrusel = () => {
-  const images = [
-    "https://cdn.pixabay.com/photo/2023/05/15/14/35/cat-7995231_640.jpg",
-    "https://cdn.pixabay.com/photo/2013/04/01/03/45/cat-98359_960_720.jpg",
-    "https://c.pxhere.com/photos/2e/93/cat_pet_cat_face_black_and_white_eyes_animal_feline_black_and_white_cat-910723.jpg!d",
-    "https://lh3.googleusercontent.com/proxy/hKvTF6K8b6o5nkw80QEQDA9aq4WHK_wAzuxJVzIx7V4DTryvtqK1oRLiBmAyXPaZHZiFM-gJPakepTUSZpBQfFJdOWJV_g",
-  ];
-
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  // Cambiar automáticamente las imágenes cada 3 segundos
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
-    }, 3000);
-
-    // Limpia el intervalo al desmontar el componente
-    return () => clearInterval(interval);
-  }, [images.length]);
-
-  return (
-    <div className="carrusel">
-      <div
-        className="carrusel-images"
-        style={{
-          transform: `translateX(-${currentIndex * 100}%)`,
-          display: "flex",
-          transition: "transform 0.5s ease-in-out",
-        }}
-      >
-        {images.map((image, index) => (
-          <div
-            key={index}
-            style={{
-              minWidth: "100%",
-              height: "100vh",
-              backgroundImage: `url(${image})`,
-              backgroundSize: "cover",
-              backgroundPosition: "center",
-            }}
-          ></div>
-        ))}
-      </div>
-    </div>
-  );
-};
-
-export default Carrusel;*/
